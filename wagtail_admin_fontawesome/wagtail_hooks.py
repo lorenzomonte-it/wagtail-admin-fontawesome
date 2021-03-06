06a333678fb71af43d3a7a8d82520bacc788afff
Lorenzo <>
2021-03-06 11:59:04 +0100

    Initial: First Release

---
+++ wagtail_admin_fontawesome/wagtail_hooks.py
@@ -0,0 +1,14 @@
+from django.utils.html import format_html
+
+from django.templatetags.static import static
+
+from wagtail.core import hooks
+
+
+def import_wagtailfontawesome_stylesheet():
+    return format_html('<link rel="stylesheet" href="{}">', static('wagtail_admin_fontawesome/css/wagtail-admin-fontawesome.min.css'))
+
+
+admin_stylesheet_hook = 'insert_global_admin_css'
+
+hooks.register(admin_stylesheet_hook, import_wagtailfontawesome_stylesheet)
